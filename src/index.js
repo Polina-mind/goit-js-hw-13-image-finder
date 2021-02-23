c8bd3c9387dc47560bfd1c8ba342c76d3630db14
Polina-mind <popova.v.polina@gmail.com>
2021-02-23 21:17:29 +0200

    update

--- src/index.js
+++ src/index.js
@@ -4,15 +4,21 @@ import apiService from './js/apiService';
 import renderingResult from './js/renderingResult';
 import './styles.css';
 
+let page = 1;
+
 refs.searchForm.addEventListener(
   'input',
   debounce(event => {
     const inputString = event.target.value;
+    console.log(inputString);
+
+    refs.finder.innerHTML = '';
+    page = 1;
 
     if (inputString.length > 0) {
-      apiService(inputString).then(renderingResult);
+      apiService(inputString, page).then(renderingResult);
     } else {
       refs.finder.innerHTML = '';
     }
-  }, 500),
+  }, 800),
 );
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,9 +1,9 @@
-function apiService(query) {
+function apiService(query, page = 1) {
   const apiKey = '20386842-9f390c2ddb67ac927c9e310bb';
-  const page = 1;
+
   const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${query}&page=${page}&per_page=12&key=${apiKey}`;
 
-  return fetch(url, page)
+  return fetch(url)
     .then(response => response.json())
     .then(({ hits }) => hits)
     .catch(error => console.log(error));
