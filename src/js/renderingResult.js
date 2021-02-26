3bef6f8a961e8fe676c2d6ecbbb400d666bcdccc
Polina-mind <popova.v.polina@gmail.com>
2021-02-26 17:49:57 +0200

    Update

--- src/js/renderingResult.js
+++ src/js/renderingResult.js
@@ -1,8 +1,17 @@
-// import { invalidRequestAlert, noPicturesAlert } from './notification';
-import { alert } from '@pnotify/core/dist/PNotify.js';
+import * as PNotifyMobile from '@pnotify/mobile/dist/PNotifyMobile.js';
+import { defaultModules, error, alert } from '@pnotify/core/dist/PNotify.js';
+import { defaults } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/Material.css';
+import 'material-design-icons/iconfont/material-icons.css';
+// import { invalidRequestAlert, noPicturesAlert } from './js/notification';
 import refs from './refs';
 import apiService from './apiService';
 import photoCardTpl from '../js/templates/card.hbs';
+
+defaults.styling = 'material';
+defaults.icons = 'material';
+defaultModules.set(PNotifyMobile, {});
 
 function renderingResult(filteredArray) {
   if (filteredArray.length > 0) {
@@ -13,7 +22,7 @@ function renderingResult(filteredArray) {
 
   if (apiService.page <= 2) {
     // invalidRequestAlert;
-    alert({
+    error({
       text: 'Invalid request, try again!',
       delay: 2000,
     });
